a1c3054db6deef91cd647ee2944ab491d684d034
JaeanHan <jaean1999@naver.com>
2022-06-29 15:26:02 +0900

    0627 수업

--- src/main/java/web/servlet/SignupServlet.java
+++ src/main/java/web/servlet/SignupServlet.java
@@ -1,28 +1,18 @@
 package web.servlet;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/*
- * 요청의 종류
- * 1. view (HTML) 요청 -> 무조건 get 요청 (주소창)
- * 2. api(application programming interface) 요청 -> CRUD(post, get, put, delete) : Javascript (AJAX 동기 및 비동기)
- * 3. 
- * 
- */
-
-@WebServlet("/index")
-public class IndexServlet extends HttpServlet {
+@WebServlet("/signup")
+public class SignupServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(request, response);
-		
+		request.getRequestDispatcher("/WEB-INF/views/signup.jsp").forward(request, response);
 	}
 
 }
